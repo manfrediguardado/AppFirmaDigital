da56198b80c8d0eb07fee372ea38c5f6607c18e8
firma <manfrediguardado8@gmail.com>
2022-07-03 14:27:25 -0600

    Agregar archivos de proyecto.

---
+++ AppFirmaDigital/AppFirmaDigital/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using SignaturePad.Forms;
+using System.IO;
+
+namespace AppFirmaDigital
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnGuardar_Clicked(object sender, EventArgs e)
+        {
+            Stream image = await PadView.GetImageStreamAsync(SignatureImageFormat.Jpeg);
+        }
+
+        private void btnClear_Clicked(object sender, EventArgs e)
+        {
+            PadView.Clear();
+
+        }
+    }
+}
